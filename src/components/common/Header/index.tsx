d0407e43ccf2afa09c905c980b0f4bdb0dd86046
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-23 15:34:26 +0900

    feat: handle header right content

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -2,7 +2,11 @@ import style from './style.module.scss';
 import Logo from '../../../assets/logo.svg';
 import SearchBar from "../SearchBar";
 
-const Header = () => {
+interface HeaderProps {
+    children: React.ReactNode;
+}
+
+const Header = ({ children }: HeaderProps) => {
     return (
         <header className={style.header}>
             <div className={style.content}>
@@ -12,6 +16,9 @@ const Header = () => {
                         <span>NXP</span>
                     </div>
                     <SearchBar/>
+                </div>
+                <div className={style.right}>
+                    {children}
                 </div>
             </div>
         </header>
