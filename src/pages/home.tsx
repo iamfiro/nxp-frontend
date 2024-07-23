e8edea7137694249f9a528b17102fc6bdc1d1555
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-23 16:06:42 +0900

    feat: add navbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,15 @@
 import {Header} from "../components";
+import ProblemIcon from '../assets/icons/problem.png';
+import QuestIcon from '../assets/icons/quest.png';
+// import CommunityIcon from '../assets/icons/community.png';
 
 const PageHome = () => {
     return (
         <>
-            <Header />
+            <Header>
+                <Header.NavItem name={'문제'} href={'/'} icon={ProblemIcon} />
+                <Header.NavItem name={'퀘스트'} href={'/'} icon={QuestIcon} />
+            </Header>
             Hello, World!
         </>
     )
