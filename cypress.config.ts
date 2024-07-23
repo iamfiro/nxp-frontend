2f4a252a023a0f2b5fddb13cc75e5c0c7cec9662
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-23 17:33:26 +0900

    feat: edit cypress chromeWebSecurity to false

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:8080",
-  },
+      e2e: {
+            baseUrl: "http://localhost:8080",
+      },
+     chromeWebSecurity: false
 });
