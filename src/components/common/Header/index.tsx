fd8695bf7a96119344bd91941a4cea9967c6c61b
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-23 15:28:25 +0900

    feat: Add SearchBar component in header

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,13 +1,17 @@
 import style from './style.module.scss';
 import Logo from '../../../assets/logo.svg';
+import SearchBar from "../SearchBar";
 
 const Header = () => {
     return (
         <header className={style.header}>
             <div className={style.content}>
-                <div className={style.logoContainer}>
-                    <img src={Logo} alt="NXP Logo" className={style.logo}/>
-                    <span>NXP</span>
+                <div className={style.left}>
+                    <div className={style.logoContainer}>
+                        <img src={Logo} alt="NXP Logo" className={style.logo}/>
+                        <span>NXP</span>
+                    </div>
+                    <SearchBar/>
                 </div>
             </div>
         </header>
