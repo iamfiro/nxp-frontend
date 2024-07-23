7682683257de18426563ab25234d7d855aeb4f65
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-23 18:17:04 +0900

    chore: add framwork setting in cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-      e2e: {
-            baseUrl: "http://localhost:8080",
-      },
-     chromeWebSecurity: false
+  e2e: {
+    baseUrl: "http://localhost:8080",
+  },
+
+  chromeWebSecurity: false,
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
 });
