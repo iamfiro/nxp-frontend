3d257e742c15a0e6716a6062b393479d61c029b8
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-24 21:36:53 +0900

    feat: add searchBar component to index.ts for global import

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './common/Header';
 export { default as Button } from './common/Button';
 export { default as Banner } from './Banner';
+export { default as SearchBar } from './SearchBar';
