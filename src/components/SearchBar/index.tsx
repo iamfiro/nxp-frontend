7bca515e292e4597e68dc1c5f15072d599db62a7
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-25 22:00:11 +0900

    refactor: 검색 컴포넌트에 placeholder props 추가

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -6,13 +6,13 @@ import { IoSearchSharp } from "react-icons/io5";
  * 문제의 제목이나 내용을 입력할 수 있는 검색 창과 아이콘을 포함한 컴포넌트
  * @returns {JSX.Element} SearchBar 컴포넌트
  */
-const SearchBar = (): JSX.Element => {
+const SearchBar = ({ placeholder }: { placeholder: string }): JSX.Element => {
     return (
         <div className={style.container}>
             <input
                 className={style.searchBar}
                 type="text"
-                placeholder="문제의 제목이나 내용을 입력하세요"
+                placeholder={placeholder}
             />
             <IoSearchSharp className={style.searchIcon} />
         </div>
