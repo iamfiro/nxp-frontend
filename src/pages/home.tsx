707c42b68317113862029245296495ed9637d7aa
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-25 13:58:34 +0900

    feat: add ProblemTable in home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import {Banner, Button, Column, Header, Row, SearchBar, Selector} from "../components";
+import {Banner, Button, Column, Header, ProblemTable, Row, SearchBar, Selector} from "../components";
 import {ButtonSize, ButtonType} from "../types/component.ts";
 import style from '../styles/pages/home.module.scss';
 import {OptionsLanguage, OptionsLevel, OptionsSort} from "../constant/select.ts";
@@ -26,6 +26,26 @@ const PageHome = () => {
                         <Selector options={OptionsSort} onChange={() => {}} styles={{ minWidth: '160px'}} />
                     </Row>
                     <h3>230 문제</h3>
+                    <ProblemTable data={[
+                        {
+                            level: 1,
+                            title: '가장 많이 받은 선물',
+                            solved: 1234,
+                            ratio: 53,
+                        },
+                        {
+                            level: 2,
+                            title: '도넛과 막대 그래프',
+                            solved: 2345,
+                            ratio: 64,
+                        },
+                        {
+                            level: 3,
+                            title: 'k진수에서 소수 개수 구하기',
+                            solved: 3456,
+                            ratio: 75,
+                        },
+                    ]} />
                 </Column>
                 <section className={style.problemRight}>
 
