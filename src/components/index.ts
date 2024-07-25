71d6afb472c21c78ed743de6bfcee21143794e59
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-25 13:12:37 +0900

    chore: divided export code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,10 @@
-export { default as Header } from './common/Header';
-export { default as Button } from './common/Button';
 export { default as Banner } from './Banner';
 export { default as SearchBar } from './SearchBar';
+export { default as ProblemTable } from './ProblemTable';
+
+// Common Components
 export { default as Row } from './common/Row';
 export { default as Column } from './common/Column';
 export { default as Selector } from './common/Selector';
+export { default as Header } from './common/Header';
+export { default as Button } from './common/Button';
