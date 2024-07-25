fe11b97cbcfee3e27928018c1a7d8f713b16db0a
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-25 15:29:34 +0900

    refactor: ProblemTable component
    add JSDoc
    move LevelToTextColor to lib/color.ts

--- src/components/ProblemTable/index.tsx
+++ src/components/ProblemTable/index.tsx
@@ -1,24 +1,14 @@
 import style from './style.module.scss';
-import {ProblemType} from "../../types/component.ts";
+import { ProblemType } from '../../types/component';
+import {levelToTextColor} from "../../lib/color.ts";
 
-function LevelToTextColor(level: number) {
-    switch (level) {
-        case 1:
-            return '#00b0f0';
-        case 2:
-            return '#00b050';
-        case 3:
-            return '#ffc000';
-        case 4:
-            return '#ff0000';
-        case 5:
-            return '#7030a0';
-        default:
-            return '#000000';
-    }
-}
-
-const ProblemTable = ({ data }: { data: ProblemType[] }) => {
+/**
+ * 문제 테이블을 렌더링하는 컴포넌트입니다.
+ * @param {Object} props - 컴포넌트 속성.
+ * @param {ProblemType[]} props.data - 문제 데이터 배열.
+ * @returns {JSX.Element} - 렌더링된 테이블 컴포넌트.
+ */
+const ProblemTable = ({ data }: { data: ProblemType[] }): JSX.Element => {
     return (
         <table className={style.table}>
             <thead>
@@ -30,21 +20,19 @@ const ProblemTable = ({ data }: { data: ProblemType[] }) => {
                 </tr>
             </thead>
             <tbody>
-            {
-                data.map((problem, index) => {
-                    return (
-                        <tr key={index} className={style.dataContainer}>
-                            <td className={style.level} style={{ color: LevelToTextColor(problem.level)}}>Lv. {problem.level}</td>
-                            <td>{problem.title}</td>
-                            <td className={style.solved}>{problem.solved}명</td>
-                            <td className={style.ratio}>{problem.ratio}%</td>
-                        </tr>
-                    )
-                })
-            }
+                {data.map((problem, index) => (
+                    <tr key={index} className={style.dataContainer}>
+                        <td className={style.level} style={{ color: levelToTextColor(problem.level) }}>
+                            Lv. {problem.level}
+                        </td>
+                        <td>{problem.title}</td>
+                        <td className={style.solved}>{problem.solved}명</td>
+                        <td className={style.ratio}>{problem.ratio}%</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
-    )
+    );
 };
 
 export default ProblemTable;
