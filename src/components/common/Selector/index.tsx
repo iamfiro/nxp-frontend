f0f50f44c3b661b0df1a3155e393412e508843d8
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-25 11:53:25 +0900

    feat: delete unused component

--- src/components/common/Selector/index.tsx
+++
@@ -1,9 +0,0 @@
-import style from './style.module.scss';
-
-const Selector = () => {
-    return (
-        <select />
-    )
-}
-
-export default Selector;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export { default as Header } from './common/Header';
 export { default as Button } from './common/Button';
 export { default as Banner } from './Banner';
 export { default as SearchBar } from './SearchBar';
-export { default as Selector } from './common/Selector';
 export { default as Row } from './common/Row';
 export { default as Column } from './common/Column';
