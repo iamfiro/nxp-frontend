c282842e39bd64278e7cb3d880c02c368cb07d36
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-25 13:06:07 +0900

    feat: add Selector component in home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,7 @@
 import {Banner, Button, Column, Header, Row, SearchBar, Selector} from "../components";
-import {ButtonSize, ButtonType, OptionType} from "../types/component.ts";
+import {ButtonSize, ButtonType} from "../types/component.ts";
 import style from '../styles/pages/home.module.scss';
-
-const category: OptionType[] = [
-    {
-        label: '전체',
-        value: 'all'
-    }
-]
-
+import {OptionsLanguage, OptionsLevel, OptionsSort} from "../constant/select.ts";
 
 const PageHome = () => {
     return (
@@ -27,9 +20,12 @@ const PageHome = () => {
             <div className={style.problemContainer}>
                 <Column className={style.problemLeft}>
                     <SearchBar />
-                    <Row>
-
+                    <Row style={{ gap: '10px' }}>
+                        <Selector options={OptionsLevel} onChange={() => {}} styles={{ minWidth: '130px'}} />
+                        <Selector options={OptionsLanguage} onChange={() => {}} styles={{ minWidth: '180px'}} />
+                        <Selector options={OptionsSort} onChange={() => {}} styles={{ minWidth: '160px'}} />
                     </Row>
+                    <h3>230 문제</h3>
                 </Column>
                 <section className={style.problemRight}>
 
