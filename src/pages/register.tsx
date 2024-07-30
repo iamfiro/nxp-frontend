3e369ef65949e1578bff34ca38a0435fc6d74a3d
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-30 22:33:52 +0900

    refactor: 회원가입 state 여러개를 하나로 변경

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,18 +1,29 @@
 import ServiceLogo from '../../public/logo.svg';
 import {AuthForm, Row} from "../components";
 import {useState} from "react";
+import {IRegister} from "../types/auth.ts";
 
 // TODO: 로고 클릭 구현
 
 const PageRegister = () => {
-	const [id, setId] = useState<string>('');
-	const [password, setPassword] = useState<string>('');
-	const [passwordConfirm, setPasswordConfirm] = useState<string>('');
-	const [phone, setPhone] = useState<string>('');
-	function handleLogin() {
+	const [data, setData] = useState<IRegister>({
+		id: '',
+		password: '',
+		passwordConfirm: '',
+		phone: {
+			number: ''
+		}
+	});
+
+	function handleRegister() {
 		// TODO: 로그인 구현
-		// id = 사용자가 입력한 아이디 또는 전화번호
-		// password = 사용자가 입력한 비밀번호
+		// data.id = 사용자가 입력한 아이디 또는 전화번호
+		// data.password = 사용자가 입력한 비밀번호
+		// data.phone.number = 사용자가 입력한 전화번호
+		// data.passwordConfirm = 사용자가 입력한 비밀번호 확인
+
+		// password와 passwordConfirm이 일치하는지 확인
+
 		// 로그인 성공 시, 메인 페이지로 이동
 		// 서버 통신은 Axios 라이브러리를 사용
 	}
@@ -36,25 +47,25 @@ const PageRegister = () => {
 			<AuthForm.Title>NXP 가입하기</AuthForm.Title>
 			<AuthForm.Input
 				placeholder={'아이디'}
-				onChange={(e) => setId(e.target.value)}
+				onChange={(e) => setData({ ...data, id: e.target.value })}
 			/>
 			<AuthForm.Phone
 				placeholder={'전화번호'}
-				onChange={(e) => setPhone(e.target.value)}
-				setState={setPhone}
-				state={phone}
+				onChange={(e) => setData({ ...data, phone: { number: e.target.value } })}
+				setState={setData}
+				state={data}
 			/>
 			<AuthForm.Input
 				placeholder={'비밀번호'}
 				password
-				onChange={(e) => setPassword(e.target.value)}
+				onChange={(e) => setData({ ...data, password: e.target.value })}
 			/>
 			<AuthForm.Input
 				placeholder={'비밀번호 확인'}
 				password
-				onChange={(e) => setPasswordConfirm(e.target.value)}
+				onChange={(e) => setData({ ...data, passwordConfirm: e.target.value })}
 			/>
-			<AuthForm.Submit onClick={() => handleLogin()}>
+			<AuthForm.Submit onClick={() => handleRegister()}>
 				가입하기
 			</AuthForm.Submit>
 			<Row style={{width: '100%', justifyContent: 'space-between', marginTop: '15px'}}>
