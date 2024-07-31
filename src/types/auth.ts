0bbc6b6be4748a0d81f964609b397243548a5f64
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-31 15:31:51 +0900

    type: 회원가입 State Interface에 Turnstile 추가

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,4 +2,8 @@ export interface IRegister {
 	id: string;
 	password: string;
 	passwordConfirm: string;
+	turnstile: {
+		state: 'error' | 'expired' | 'solved' | 'idle';
+		token?: string | undefined;
+	}
 }
