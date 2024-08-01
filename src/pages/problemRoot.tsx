50ffa2e19cea9a4dd39b2f6a05cd7279fee39e74
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-01 11:23:42 +0900

    feat: problem 루트 페이지에 접속하면 '/'으로 리다이렉트

--- src/pages/problemRoot.tsx
+++ src/pages/problemRoot.tsx
@@ -1,4 +1,10 @@
+import {redirect} from "react-router-dom";
+import {useEffect} from "react";
+
 const PageRootProblem = () => {
+	useEffect(() => {
+		redirect('/');
+	}, [])
 	return (
 		<span>잠시만 기다려주세요</span>
 	)
