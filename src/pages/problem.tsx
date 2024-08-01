51cd738ebd9f0128021d0f85d6cc22ca02934141
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-01 14:24:24 +0900

    feat: 문제 페이지 추가

--- src/pages/problem.tsx
+++ src/pages/problem.tsx
@@ -1,7 +1,55 @@
-const PageProblem = () => {
-	return (
-		<main>
+import style from '../styles/pages/problem.module.scss';
+import {useState} from "react";
+import ServiceLogo from '../assets/logo.svg';
+import {Column, MonacoEditor, Row} from "../components";
+import { SiDocsdotrs } from "react-icons/si";
+import { FaCirclePlay } from "react-icons/fa6";
+import { MdCloudUpload } from "react-icons/md";
 
+const PageProblem = () => {
+	const [code, setCode] = useState<string>(
+		'function add(a, b) {'
+	);
+	const [language, setLanguage] = useState<string>('java');
+
+	return (
+		<main className={style.container}>
+			<header className={style.header}>
+				<img src={ServiceLogo} alt={'Logo'} className={style.logo}/>
+				<button className={style.submit}><MdCloudUpload size={20} /> 코드 제출하기</button>
+			</header>
+			<section className={style.main}>
+				<div className={style.info}>
+					<Row className={style.infoHeader}>
+						<div className={style.infoTab} data-selected={true}>
+							<SiDocsdotrs />
+							문제
+						</div>
+						<div className={style.infoTab} data-selected={false}>
+							<FaCirclePlay />
+							채점 내역
+						</div>
+					</Row>
+					<div className={style.infoContent}>
+						<h1>문제 제목</h1>
+						<p>문제 내용</p>
+					</div>
+				</div>
+				<Column style={{ width: '50%' }}>
+					<div className={style.codeHeader}>
+						<span>
+							solution.{language}
+						</span>
+						<select className={style.selectLanguage} value={language} onChange={(e) => setLanguage(e.target.value)}>
+							<option value={'c'}>C</option>
+							<option value={'cpp'}>C++</option>
+							<option value={'java'}>Java</option>
+							<option value={'go'}><img src={'/icon/lang/GoLang.svg'} />GoLang</option>
+						</select>
+					</div>
+					<MonacoEditor code={code} setCode={setCode} language={language}/>
+				</Column>
+			</section>
 		</main>
 	)
 }
