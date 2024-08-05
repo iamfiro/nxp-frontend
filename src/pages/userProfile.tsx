5249ea811436aad25ba5e7ed10a05b6602aa6033
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-05 20:17:50 +0900

    feat: userProfile.tsx에 사용자 이름 추가

--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -8,6 +8,7 @@ const PageUserProfile = () => {
 			<main className={style.container}>
 				<section className={style.profile}>
 					<img src={'https://avatars.githubusercontent.com/u/72495729?v=4'} className={style.profileImage} alt={'프로필 사진'} />
+					<h1 className={style.profileName}>iamfiro</h1>
 				</section>
 			</main>
 		</>
