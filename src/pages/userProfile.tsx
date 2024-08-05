2d3035db3fa3160c8071f151b2f70e5fe30ae00e
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-05 20:17:21 +0900

    feat: userProfile.tsx에 프로필 이미지 추가

--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -1,8 +1,15 @@
 import style from '../styles/pages/userProfile.module.scss';
+import TemplateHeader from "../template/header.tsx";
 
 const PageUserProfile = () => {
 	return (
 		<>
+			<TemplateHeader />
+			<main className={style.container}>
+				<section className={style.profile}>
+					<img src={'https://avatars.githubusercontent.com/u/72495729?v=4'} className={style.profileImage} alt={'프로필 사진'} />
+				</section>
+			</main>
 		</>
 	)
 }
