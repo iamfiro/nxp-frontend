d799910278e67adbd36e7d246c85660030505da7
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-05 20:17:57 +0900

    feat: userProfile.tsx에 사용자 설명 추가

--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -9,6 +9,10 @@ const PageUserProfile = () => {
 				<section className={style.profile}>
 					<img src={'https://avatars.githubusercontent.com/u/72495729?v=4'} className={style.profileImage} alt={'프로필 사진'} />
 					<h1 className={style.profileName}>iamfiro</h1>
+
+					<p>
+						안녕하세요! iamfiro입니다. 프로그래밍을 공부하고 있습니다.
+					</p>
 				</section>
 			</main>
 		</>
