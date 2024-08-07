1accd7f00d2554e42f7b190a254acb990d161248
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-07 17:06:57 +0900

    feat: acToken 만료시 refreshToken으로 재발급 하는 로직 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -22,3 +22,28 @@ request.interceptors.request.use(async function (config) {
 }, function (error) {
 	return Promise.reject(error);
 })
+
+request.interceptors.response.use(async function (response) {
+	return response;
+}, async function (error) {
+	const { config, response: { status }} = error;
+
+	if(status === 401 && error.data.message === 'InvaildTokenException') {
+		// 토큰이 유효하지 않을 때 로그안 페이지로 이동
+		window.location.hash = '/login';
+	}
+
+	if(status === 401 && error.data.message === 'TokenExpired') {
+		// 토큰 갱신 요청
+		const tokenRefreshResponse = await request.post('/auth/refresh');
+
+		if(tokenRefreshResponse.status === 200) {
+			const { accessToken, refreshToken } = tokenRefreshResponse.data;
+			// 새로 발급받은 토큰들을 쿠키에 저장
+			Cookie.set('acToken', accessToken);
+			Cookie.set('rfToken', refreshToken);
+			// 토큰 갱신 후 기존 요청 재시도
+			return request(config);
+		}
+	}
+})
