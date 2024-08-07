3fd8a3f2e294bdbe5b62116aedece7a871fa0428
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-07 12:16:01 +0900

    feat: 커스텀 axios 인스턴스 개발

---
+++ src/lib/axios.ts
@@ -0,0 +1,7 @@
+import axios, {AxiosInstance} from "axios";
+
+const SERVER_ADDRESS = import.meta.env.VITE_API_URL as string;
+
+export const request: AxiosInstance = axios.create({
+	baseURL: SERVER_ADDRESS,
+})
