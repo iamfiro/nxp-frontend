01759596378f7464d31eec1a732325c2b223fdb7
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-07 18:12:18 +0900

    feat: indexedDB Auth 관리 함수 추가

---
+++ src/lib/idb.ts
@@ -0,0 +1,35 @@
+// src/idb.ts
+import { openDB, IDBPDatabase } from 'idb';
+
+const DB_NAME = 'auth-db';
+const STORE_NAME = 'tokens';
+
+async function createStore(): Promise<IDBPDatabase<unknown>> {
+	return await openDB(DB_NAME, 1, {
+		upgrade(db) {
+			if (!db.objectStoreNames.contains(STORE_NAME)) {
+				db.createObjectStore(STORE_NAME, {
+				  	keyPath: 'key',
+				});
+			}
+		},
+	});
+}
+
+export async function saveAccessToken(token: string): Promise<void> {
+	const db = await createStore();
+	await db.put(STORE_NAME, { key: 'accessToken', value: token });
+}
+
+export async function getAccessToken(): Promise<string | null> {
+	const db = await createStore();
+	// Access token 조회
+	const tokenData = await db.get(STORE_NAME, 'accessToken');
+	return tokenData ? tokenData.value : null;
+}
+
+export async function deleteAccessToken(): Promise<void> {
+	const db = await createStore();
+	// Access token	삭제
+	await db.delete(STORE_NAME, 'accessToken');
+}
