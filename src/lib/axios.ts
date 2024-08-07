7de492dabf0630ef2323ff9e8cf6007db595178c
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-07 17:06:25 +0900

    feat: axios.ts 요청시 acToken 추가 하는 interceptor 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,24 @@
 import axios, {AxiosInstance} from "axios";
+import Cookie from "js-cookie";
 
 const SERVER_ADDRESS = import.meta.env.VITE_API_URL as string;
 
 export const request: AxiosInstance = axios.create({
 	baseURL: SERVER_ADDRESS,
 	headers: {
-		Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
 		"Content-Type": "application/json",
 	},
 	timeout: 5000,
+});
+
+request.interceptors.request.use(async function (config) {
+	const acessToken = Cookie.get('acToken');
+
+	if(acessToken) {
+		config.headers.Authorization = `Bearer ${acessToken}`;
+	}
+
+	return config;
+}, function (error) {
+	return Promise.reject(error);
 })
