f7fd18ef6c273210ff6c5eef689dd043c0c0daeb
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-07 18:40:54 +0900

    feat: 헤더 로그인, 가입하기 버튼 navigate 추가

--- src/template/header.tsx
+++ src/template/header.tsx
@@ -1,14 +1,20 @@
 import {Button, Header, Row} from "../components";
 import {ButtonSize, ButtonType} from "../types/component.ts";
+import {useNavigate} from "react-router-dom";
 
 const TemplateHeader = () => {
+	const navigate = useNavigate();
 	return (
 		<Header>
 			<Row style={{ gap: '5px' }}>
-				<Button onClick={() => {}} size={ButtonSize.Small} type={ButtonType.White}>
+				<Button onClick={() => {
+					navigate('/login');
+				}} size={ButtonSize.Small} type={ButtonType.White}>
 				로그인
 				</Button>
-				<Button onClick={() => {}} size={ButtonSize.Small} type={ButtonType.Primary}>
+				<Button onClick={() => {
+					navigate('/register');
+				}} size={ButtonSize.Small} type={ButtonType.Primary}>
 					가입하기
 				</Button>
 			</Row>
