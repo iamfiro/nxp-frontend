c8c16fc1693adc8368b84b0b9c28b231409fda32
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-08 14:16:37 +0900

    feat: Streak 컴포넌트 API fetch 구현하기

--- src/components/Streak/index.tsx
+++ src/components/Streak/index.tsx
@@ -1,15 +1,35 @@
 import style from './style.module.scss';
 import { PiPlantFill } from "react-icons/pi";
 import {Row} from "../index.ts";
+import {useEffect, useState} from "react";
+import {request} from "../../lib/axios.ts";
 
 const Streak = () => {
+	const [streak, setStreak] = useState(0);
+	const [isLoaded, setIsLoaded] = useState(false);
+
+	useEffect(() => {
+		request('/streak').then((response) => {
+			setStreak(response.data.streak);
+			setIsLoaded(true);
+		});
+	}, []);
+
 	return (
 		<div className={style.container}>
 			<Row style={{ gap: '8px' }}>
 				<PiPlantFill size={20} />
 				<span className={style.title}>스트릭</span>
 			</Row>
-			<span className={style.value}>현재 <strong>43</strong>일</span>
+			<span className={style.value}>
+				{
+					isLoaded ? (
+						<span>
+							현재 <strong>{streak}</strong>일
+						</span>
+					) : '불러오는 중'
+				}
+			</span>
 		</div>
 	)
 };
