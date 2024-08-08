3d9a78cad11810df755ff083441deeeda43b35f1
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-08 12:32:44 +0900

    refactor: Avatar 컴포넌트에 classname 인자 추가

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -2,11 +2,12 @@ interface AvatarProps {
 	src: string;
 	alt: string;
 	size: number;
+	className?: string;
 }
 
-const Avatar = ({ src, alt, size }: AvatarProps) => {
+const Avatar = ({ src, alt, size, className }: AvatarProps) => {
 	return (
-		<img src={src} alt={alt} width={size} height={size} style={{ borderRadius: '100px'}} />
+		<img src={src} alt={alt} width={`${size}px`} className={className} height={`${size}px`} style={{ borderRadius: '100px'}} />
 	);
 };
 
