b74ca3d4ed2dd8b3027f4d6fa6786a1ec597d8de
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-08 13:42:30 +0900

    feat: Skeleton 컴포넌트에 skeletonStyle 인자 추가

--- src/components/common/Skeleton/index.tsx
+++ src/components/common/Skeleton/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import styles from './style.module.scss';
 
 interface SkeletonProps {
     width?: string;
     height?: string;
     borderRadius?: string;
+	skeletonStyle?: CSSProperties
 }
 
-const Skeleton: React.FC<SkeletonProps> = ({ width, height, borderRadius }) => {
+const Skeleton: React.FC<SkeletonProps> = ({ width, height, borderRadius, skeletonStyle }) => {
     const style = {
         width: width || '100%',
         height: height || '20px',
         borderRadius: borderRadius || '4px',
+		...skeletonStyle
     };
 
     return <div className={styles.skeleton} style={style}></div>;
