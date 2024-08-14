ff8e25268893af560038c7656dc5f7230ec54ad0
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-14 18:24:50 +0900

    feat: idb내 메모 관리 함수 추가

--- src/lib/idb.ts
+++ src/lib/idb.ts
@@ -3,6 +3,7 @@ import { openDB, IDBPDatabase } from 'idb';
 const DB_NAME = 'auth-db';
 const TOKEN_STORE_NAME = 'tokens';
 const IS_LOGGED_IN_STORE_NAME = 'isLoggedIn';
+const MEMO_STORE_NAME = 'problem_memo';
 
 async function createStore(): Promise<IDBPDatabase<unknown>> {
 	return await openDB(DB_NAME, 1, {
@@ -55,4 +56,35 @@ export async function deleteIsLoggedIn(): Promise<void> {
 	const db = await createStore();
 	// 로그인 여부 삭제
 	await db.delete(IS_LOGGED_IN_STORE_NAME, 'isLoggedIn');
+}
+
+interface Memo {
+	id: string;
+	content: string;
+}
+
+export async function UpSertMemo(memo: Memo): Promise<void> {
+	const db = await createStore();
+	const existingMemo = await db.get(MEMO_STORE_NAME, memo.id);
+
+	if (existingMemo) {
+		// Update existing memo
+		await db.put(MEMO_STORE_NAME, {
+			...existingMemo,
+			...memo,
+			updatedAt: new Date(),
+		});
+	} else {
+		// Insert new memo
+		await db.add(MEMO_STORE_NAME, {
+			...memo,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		});
+	}
+}
+
+export async function getMemo(id: string): Promise<Memo | undefined> {
+	const db = await createStore();
+	return await db.get(MEMO_STORE_NAME, id);
 }
