dffdb7e6cfce00249bb56f744cb99178d820cebd
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-14 17:30:17 +0900

    style: 문제 페이지 타이틀 추가

--- src/pages/problem.tsx
+++ src/pages/problem.tsx
@@ -20,6 +20,11 @@ const PageProblem = () => {
 		<main className={style.container}>
 			<TemplateHeader />
 
+			<Row className={style.main}>
+				<section className={style.left}>
+					<h1 className={style.title}>가장 많이 받은 선물</h1>
+				</section>
+			</Row>
 		</main>
 	)
 }
