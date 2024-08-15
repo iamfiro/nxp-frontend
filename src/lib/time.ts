f6254d7c9de40aabd1c12f5565b30cea60fee3f5
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-15 16:10:45 +0900

    style: global.scss에 yellow 색상 추가

---
+++ src/lib/time.ts
@@ -0,0 +1,18 @@
+export const elapsedTime = (date: number): string => {
+	const start = new Date(date);
+	const end = new Date();
+
+	const seconds = Math.floor((end.getTime() - start.getTime()) / 1000);
+	if (seconds < 60) return '방금 전';
+
+	const minutes = seconds / 60;
+	if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+
+	const hours = minutes / 60;
+	if (hours < 24) return `${Math.floor(hours)}시간 전`;
+
+	const days = hours / 24;
+	if (days < 7) return `${Math.floor(days)}일 전`;
+
+	return `${start.toLocaleDateString()}`;
+};
