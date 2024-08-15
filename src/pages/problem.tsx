69e4c00f191edf9997e2c2d8eedaae7eada235c8
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-16 08:28:17 +0900

    fix: 문제 페이지 헤더 높이 짜부되는 문제 해겲

--- src/pages/problem.tsx
+++ src/pages/problem.tsx
@@ -79,6 +79,7 @@ const PageProblem = () => {
 	);
 	const [editorLanguage, setEditorLanguage] = useState<string>(localStorage.getItem('recentLanguage') || 'c');
 
+	// 메모 불러오기
     useEffect(() => {
         // Fetch the memo content when the component mounts
         const fetchMemo = async () => {
@@ -134,6 +135,7 @@ const PageProblem = () => {
 		fetchCode();
 	}, [isSubmitModalOpen, pathname]);
 
+	// 커스텀 메타 태그 설정
     setMetaTag({
         title: "두 수 정하기 - NXP",
         description: "두 수를 입력받아 더하는 문제",
@@ -187,138 +189,145 @@ const PageProblem = () => {
 	}
 
     return (
-        <main className={style.container}>
-            <TemplateHeader />
-            <Row className={style.main}>
-                <section className={style.left}>
-                    <h1 className={style.title}>가장 많이 받은 선물</h1>
-                </section>
-                <Column className={style.right}>
-                    <button className={style.submit} onClick={() => handleMainSubmitButton()}>
-                        <FaCode size={18}/> 코드 제출하기
-                    </button>
-                    <Row style={{gap: '10px', marginTop: '15px'}}>
-                        <section className={style.problemLimit}>
-                            <Row className={style.limitTitle}>
-                                <MdOutlineTimelapse/>
-                                <span>시간 제한</span>
-                            </Row>
-                            <span className={style.limitValue}>1초</span>
-                        </section>
-                        <section className={style.problemLimit}>
-                            <Row className={style.limitTitle}>
-                                <FaMemory/>
-                                <span>메모리 제한</span>
-                            </Row>
-                            <span className={style.limitValue}>128 MB</span>
-                        </section>
-                    </Row>
-                    <ul className={style.problemSummaryContainer}>
-                        <ProblemSummary title={'제출 수'} value={'1,000명'} />
-                        <ProblemSummary title={'정답 수'} value={'1,000명'} />
-                        <ProblemSummary title={'실패 수'} value={'22,293명'} />
-                    </ul>
-
-                    <span className={style.memoTitle}>메모</span>
-                    <textarea
-                        className={style.memo}
-                        placeholder={'여기를 눌러 메모를 입력하세요'}
-						defaultValue={memoContent}
-                        onChange={(e) => setMemoContent(e.target.value)}
-                    />
-
-					<Row style={{justifyContent: 'space-between', alignItems: 'center', marginTop: '40px'}}>
-						<span className={style.submitTitle}>제출 현황</span>
-						<button className={style.submitUpdate} onClick={() => handleUpdateSubmitStatus()}>
-							{
-								isSubmitStatusPending ? (
-									<>
-										<MdOutlineRefresh size={16} className={'spinning'} />
-									</>
-								) : (
-									<>
-										<MdOutlineRefresh size={16} /> 눌러서 업데이트
-									</>
-								)
-							}
+		<>
+			<TemplateHeader/>
+			<main className={style.container}>
+				<Row className={style.main}>
+					<section className={style.left}>
+						<h1 className={style.title}>가장 많이 받은 선물</h1>
+					</section>
+					<Column className={style.right}>
+						<button className={style.submit} onClick={() => handleMainSubmitButton()}>
+							<FaCode size={18}/> 코드 제출하기
 						</button>
-					</Row>
-					<Column className={style.submitContainer}>
-						<Submit
-							status={'pending'}
-							language={'C99'}
-							date={'2024-8-15 3:33:42'}
-							memory={5592}
-							time={192}
-						/>
-						<Submit
-							status={'success'}
-							language={'C99'}
-							date={'2024-8-15 3:33:42'}
-							memory={5592}
-							time={192}
-						/>
-						<Submit
-							status={'fail'}
-							language={'C99'}
-							date={'2024-8-15 3:33:42'}
-							memory={5592}
-							time={192}
-						/>
-						<Submit
-							status={'fail'}
-							language={'C99'}
-							date={'2024-8-15 3:33:42'}
-							memory={5592}
-							time={192}
-						/>
-					</Column>
-				</Column>
-			</Row>
-			{isSubmitModalOpen && (
-				createPortal(
-					<>
-						<Modal.Backdrop isVisible={isSubmitModalOpen} handleClose={() => setIsSubmitModalOpen(false)}>
-							<Modal className={style.submitModal}>
-								<Row className={style.submitModalHeader}>
-									<h2>코드 제출하기</h2>
-									<button className={style.modalClose} onClick={() => setIsSubmitModalOpen(false)}>
-										<IoCloseSharp size={20} />
-									</button>
+						<Row style={{gap: '10px', marginTop: '15px'}}>
+							<section className={style.problemLimit}>
+								<Row className={style.limitTitle}>
+									<MdOutlineTimelapse/>
+									<span>시간 제한</span>
 								</Row>
-								<MonacoEditor code={editorCode} setCode={setEditorCode} language={editorLanguage}/>
-								<Selector initialValue={editorLanguage} styles={{ margin: 'auto 0 10px 0', height: '45px'}} className={style.select} options={[
-									{
-										label: 'C',
-										value: 'c'
-									},
-									{
-										label: 'C++',
-										value: 'cpp'
-									},
-									{
-										label: 'Java',
-										value: 'java'
-									},
-									{
-										label: 'GoLang',
-										value: 'go'
-									},
-									{
-										label: 'Rust',
-										value: 'rust'
-									}
-								]} onChange={(e) => setEditorLanguage(e)} />
-								<button className={style.submit} style={{width: '100%'}} onClick={() => handleModalSubmit()}>
-									<FaCode size={18} /> 제출하기
-								</button>
-							</Modal>
-						</Modal.Backdrop>
-					</>,
-					document.body
-				)
-			)}
-		</main>
+								<span className={style.limitValue}>1초</span>
+							</section>
+							<section className={style.problemLimit}>
+								<Row className={style.limitTitle}>
+									<FaMemory/>
+									<span>메모리 제한</span>
+								</Row>
+								<span className={style.limitValue}>128 MB</span>
+							</section>
+						</Row>
+						<ul className={style.problemSummaryContainer}>
+							<ProblemSummary title={'제출 수'} value={'1,000명'}/>
+							<ProblemSummary title={'정답 수'} value={'1,000명'}/>
+							<ProblemSummary title={'실패 수'} value={'22,293명'}/>
+						</ul>
+
+						<span className={style.memoTitle}>메모</span>
+						<textarea
+							className={style.memo}
+							placeholder={'여기를 눌러 메모를 입력하세요'}
+							defaultValue={memoContent}
+							onChange={(e) => setMemoContent(e.target.value)}
+						/>
+
+						<Row style={{justifyContent: 'space-between', alignItems: 'center', marginTop: '40px'}}>
+							<span className={style.submitTitle}>제출 현황</span>
+							<button className={style.submitUpdate} onClick={() => handleUpdateSubmitStatus()}>
+								{
+									isSubmitStatusPending ? (
+										<>
+											<MdOutlineRefresh size={16} className={'spinning'}/>
+										</>
+									) : (
+										<>
+											<MdOutlineRefresh size={16}/> 눌러서 업데이트
+										</>
+									)
+								}
+							</button>
+						</Row>
+						<Column className={style.submitContainer}>
+							<Submit
+								status={'pending'}
+								language={'C99'}
+								date={'2024-8-15 3:33:42'}
+								memory={5592}
+								time={192}
+							/>
+							<Submit
+								status={'success'}
+								language={'C99'}
+								date={'2024-8-15 3:33:42'}
+								memory={5592}
+								time={192}
+							/>
+							<Submit
+								status={'fail'}
+								language={'C99'}
+								date={'2024-8-15 3:33:42'}
+								memory={5592}
+								time={192}
+							/>
+							<Submit
+								status={'fail'}
+								language={'C99'}
+								date={'2024-8-15 3:33:42'}
+								memory={5592}
+								time={192}
+							/>
+						</Column>
+					</Column>
+				</Row>
+				{isSubmitModalOpen && (
+					createPortal(
+						<>
+							<Modal.Backdrop isVisible={isSubmitModalOpen}
+											handleClose={() => setIsSubmitModalOpen(false)}>
+								<Modal className={style.submitModal}>
+									<Row className={style.submitModalHeader}>
+										<h2>코드 제출하기</h2>
+										<button className={style.modalClose}
+												onClick={() => setIsSubmitModalOpen(false)}>
+											<IoCloseSharp size={20}/>
+										</button>
+									</Row>
+									<MonacoEditor code={editorCode} setCode={setEditorCode} language={editorLanguage}/>
+									<Selector initialValue={editorLanguage}
+											  styles={{margin: 'auto 0 10px 0', height: '45px'}}
+											  className={style.select} options={[
+										{
+											label: 'C',
+											value: 'c'
+										},
+										{
+											label: 'C++',
+											value: 'cpp'
+										},
+										{
+											label: 'Java',
+											value: 'java'
+										},
+										{
+											label: 'GoLang',
+											value: 'go'
+										},
+										{
+											label: 'Rust',
+											value: 'rust'
+										}
+									]} onChange={(e) => setEditorLanguage(e)}/>
+									<button className={style.submit} style={{width: '100%'}}
+											onClick={() => handleModalSubmit()}>
+										<FaCode size={18}/> 제출하기
+									</button>
+								</Modal>
+							</Modal.Backdrop>
+						</>,
+						document.body
+					)
+				)}
+			</main>
+		</>
 	)
 }
 
