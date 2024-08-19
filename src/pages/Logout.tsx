1d425f012875a3e0d35b6b4ee405322df8db2ea0
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-19 21:28:12 +0900

    feat: 로그아웃 기능 구현

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -2,21 +2,27 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useIsLoggined from '../hooks/useIsLoggined.ts';
 import {request} from "../lib/axios.ts";
+import useAccessToken from "../hooks/useAccessToken.ts";
+import Cookie from "js-cookie";
 
 const Logout = () => {
     const navigate = useNavigate();
     const { removeUserLogin } = useIsLoggined();
+	const { removeToken } = useAccessToken()
 
     useEffect(() => {
         const logout = async () => {
 			await request.post('/auth/logout').then(async () => {
 				await removeUserLogin();
+				await removeToken();
+				Cookie.remove('rfToken');
+
 				navigate('/login');
 			});
         };
 
         logout();
-    }, [navigate, removeUserLogin]);
+    });
 
     return (
         <div>
