0d17645476b63071d4d5f14d5303c0fc34a8c500
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-08-20 15:01:25 +0900

    refactor: Header.cy.tsx eslint 문제 해결

--- src/components/common/Header/Header.cy.tsx
+++ src/components/common/Header/Header.cy.tsx
@@ -1,6 +1,7 @@
 import Header from "./index.tsx";
 import Logo from '../../../assets/logo.svg'
 import TrophyIcon from '../../../assets/icons/trophy.png';
+import TrophyIconSelect from '../../../assets/icons/trophy-color.png'
 
 describe('<Header /> 컴포넌트 테스트', () => {
     it('로고와 서비스 이름이 렌더링 되는지 테스트', () => {
@@ -15,7 +16,7 @@ describe('<Header /> 컴포넌트 테스트', () => {
 
     it('NavItem 컴포넌트가 정상적으로 렌더링되는지 테스트', () => {
         cy.mount(
-            <Header.NavItem name={'랭킹'} href={'/rank'} icon={TrophyIcon} />
+            <Header.NavItem name={'랭킹'} href={'/rank'} icon={TrophyIcon} iconSelect={TrophyIconSelect} />
         )
         cy.get('span').contains('랭킹')
         cy.get('img').should('have.attr', 'src', TrophyIcon);
